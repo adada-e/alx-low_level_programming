f1ffa2725b2a17768726ac8a96a19970224aecd1
Whisper Edia <ediaadada@gmail.com>
2022-03-16 04:01:49 +0100

    program that prints _putchar followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,23 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
-/**
- * main - Entry point
- * This program will print _putchar and start a new line
- * Return: Always 0 (Sucess)
- */
- 
-int main(void)
+ /** 
+  * main - Entry point putchar 
+  *
+  * _putchar prints the given characters
+  *
+  * Return: Always 0 (Sucess)
+  */
+
+int main(void)   
 {
-    char text[9] = "_putchar";
-    int i;
-        for (i =0; i<9; i++)
-		{
-			_putchar(text[i])'
-				 _putchar('\n');
-						}
-    return (0);
-}      
+ 	_putchar('_');
+	_putchar('p');
+        _putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+}
+ 
