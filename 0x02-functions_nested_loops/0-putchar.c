ae9d31b1643b8ffc4b931e511c3657cf7020d80d
Whisper Edia <ediaadada@gmail.com>
2022-03-16 22:34:00 +0100

    program that prints _putchar and starts a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,15 @@
 
 int main(void)
 {
-	char *word = "_putchar";
-
-	while (*word != '\0')
-	{
-		_putchar.(*word);
-		word++;
-	}
-	_putchar('\n')
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 	return (0);
 }
