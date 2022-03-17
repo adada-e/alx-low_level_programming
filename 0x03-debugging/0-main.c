2a33eb4cbfa80dc4c3521770bb889bd7420c6506
Whisper Edia <ediaadada@gmail.com>
2022-03-17 15:05:39 +0100

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative 
+ * main - Prints the different numbers at different occurrences 
+ *
  * Return: 0
  */
-
 int main(void)
 {
-int i;
-i = 0;
+	int i;
 
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-return (0);
+	return (0);
 }
