a5aac2e069b6aa20f4ffd136447d9d17c9122179
Whisper Edia <ediaadada@gmail.com>
2022-03-18 07:45:24 +0100

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - Prints the different numbers at different occurrences 
+ * main - Prints the different numbers at different occurrences
  *
  * Return: 0
  */
 int main(void)
-{	
+{
 	int i;
- 
- 	i = 0;
- 	positive_or_negative(i);
- 
- 	return (0);
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
