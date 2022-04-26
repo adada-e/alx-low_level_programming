f466175e799c1ca022555cd6e8318cf04876bf5a
Whisper Edia <ediaadada@gmail.com>
2022-04-26 11:13:02 +0100

    a function that frees a listint_t list.

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - Frees a listint_t list
+ * @head: The node at the beginning of the list
+ */
+void free_listint(listint_t *head)
+{
+	if (head)
+	{
+		if (head->next)
+			free_listint(head->next);
+		if (head)
+			free(head);
+	}
+}
